547068802c82dddbc619c0d5314c8f38ccceb0d1
SweetieMetalhead <paul.lv.ohl@gmail.com>
2020-05-30 10:48:19 +0200

    nginx is now working on http, https and ssh <3 I also updated start.sh to include these changes and it shows funky text using figlet and lolcat

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 # Creation date: 24/05/2020
 # Description: builds docker images and deploys them onto a kubernetes cluster for the ft_service project of school 42
 
+LIGHT_BLUE="\033[1;34m"
+NC="\033[0m"
+
 # Checks the presence of docker and minikube on the system
 if ! which docker 1> /dev/null || ! which minikube 1> /dev/null; then
 	echo "Please install docker and minikube before lauching this script"
@@ -17,4 +20,16 @@ fi
 minikube addons enable ingress
 minikube addons enable metrics-server
 
+# Builds Dockerfiles
+docker build -t custom-nginx:latest srcs/nginx/
+
+# Applies kustomization
+kubectl apply -k srcs/
+
+# The result
+figlet "the result" | lolcat -F 0.4 -a
+
+# Get urls
+echo "$LIGHT_BLUE""Nginx urls:""$NC"
+minikube service nginx --url | tr "\n" "#"  | sed -E 's|^(.*#)http(.*#)http://(.*)#|\1https\2ssh://bonjour@\3|' | tr "#" "\n"
 
